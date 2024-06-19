f8da4e384b61995e3dd848d393e20564cd151f95
Tnoun23 <tinenounoute@gmail.com>
2024-06-18 20:00:42 -0700

    This is the Little Lemon App being saved on Git

---
+++ Desktop/LittleLemonNew/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Christine N. on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
